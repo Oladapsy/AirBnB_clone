e72855ed74d75dd5339f983f54d5521d6169a1f3
Oladapsy <dapoodedeyi@yahoo.com>
2024-03-06 18:35:56 +0100

    models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,4 +3,29 @@
 from models.basemodel import BaseModel
 
 class FileStorage:
-    """ Representing"""
+    """ class that serializes instances to a JSON file
+    and deserializes JSON file to instances:
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ a public instance method all that returns the dictionary
+        object(private)
+        """
+        return FileStorage.__objects
+
+    def new(self):
+        """ sets in __objects the obj with key <obj class name>.id"""
+        FileStorage.__objects[f"{} {}"]
+
+    def save(self):
+        """ serializes __objects to the JSON file (path: __file_path)"""
+
+    def reload(self):
+        """deserializes the JSON file to __objects (only if the JSON file
+        (__file_path) exists ; otherwise, do nothing. If the file doesn’t
+        exist, no exception should be raised)
+        """
+
+
