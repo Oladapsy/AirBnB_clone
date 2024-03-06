d5233912338168d1d4ef551412935e0b5ed41fcd
Oladapsy <dapoodedeyi@yahoo.com>
2024-03-06 13:58:55 +0100

    the unit test

---
+++ tests/test_base_model.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""
+    A class that test thee Base model
+    to ensure it is working properly
+    using unit test
+    """
+from models.base_model import BaseModel
+import unittest
+from datetime import datetime
+from datetime import timedelta
+from time import sleep
+
+
+class TestBaseModel_instantation(unittest.TestCase):
+    """ used to test the base model"""
+    def test_no_args_instance(self):
+        """ check if the model has no args """
+        self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_if_id_is_string(self):
+        """ test if id is a string"""
+        Bm1 = BaseModel()
+        Bid = Bm1.id
+        self.assertEqual(str, type(Bid))
+
+    def test_if_id_is_not_others(self):
+        """ test if id is not a string"""
+        Bm1 = BaseModel()
+        Bid = Bm1.id
+        self.assertNotEqual(int, type(Bid))
+        self.assertNotEqual(float, type(Bid))
+        self.assertNotEqual(list, type(Bid))
+        self.assertNotEqual(dict, type(Bid))
+        self.assertNotEqual(dict, type(Bid))
+        self.assertNotEqual(set, type(Bid))
+
+    def test_two_models_id(self):
+        """test if two object ids are not equal as we don't want it to"""
+        Bm1 = BaseModel()
+        Bm2 = BaseModel()
+        self.assertNotEqual(Bm1, Bm2)
+
+    def test_created_at(self):
+        """ let us check if current date is a datetime type"""
+        Bm1 = BaseModel()
+        time = Bm1.created_at
+        self.assertEqual(datetime, type(time))
+
+    def test_updated_at(self):
+        """ let us check if updated_at date is a datetime type"""
+        Bm1 = BaseModel()
+        time = Bm1.updated_at
+        self.assertEqual(datetime, type(time))
+
+    def test_created_at_test_future_time(self):
+        """ let us check if current date is assigned"""
+        Bm1 = BaseModel()
+        time = Bm1.created_at
+        self.assertNotEqual(type(time), timedelta(days=1))
+
+    def test_created_at_is_less_than_update_at(self):
+        """check if time created is less than time updated"""
+        Bm1 = BaseModel()
+        time = Bm1.created_at
+        Bm1.save()
+        time2 = Bm1.updated_at
+        self.assertLess(time, time2)
+
+    def test_created_at_is_less_than_new_created_at(self):
+        """check if time created is less than time the second class
+        object is created
+        """
+        Bm1 = BaseModel()
+        time = Bm1.created_at
+        sleep(1)
+        Bm2 = BaseModel()
+        time2 = Bm2.created_at
+        self.assertLess(time, time2)
+
+    def test_updated_of_two_base_class(self):
+        """ check if two classes updated at are different"""
+        Bm1 = BaseModel()
+        sleep(1)
+        Bm2 = BaseModel()
+        self.assertLess(Bm1.updated_at, Bm2.updated_at)
+
+
+if __name__ == "__main__":
+    unittest.main()
