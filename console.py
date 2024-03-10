c455a8749b4568ee49e8a0463a166dd7f382bebf
Oladapsy <dapoodedeyi@yahoo.com>
2024-03-10 15:05:18 +0100

    refactor to pep8

--- console.py
+++ console.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """A program that contains entry point of the command interpreter: """
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """The command line for the programm... powered by cmd"""
     prompt = '(hbnb) '
+
     def do_EOF(self, line):
         """The end of file which uses Ctrl D to quit the program"""
         return True
@@ -13,18 +15,21 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         """Quit command to exit the program"""
         return True
-    
+
     def emptyline(self):
         """should do nothing when the line is empty"""
         return False
 
-    def do_create(self, line):
+    def do_create(self, args):
         """Creates a new instance of BaseModel, saves it (to the JSON file)
         and prints the id. Ex: $ create BaseModel"""
-        return True
-
-
-
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            test = args[0]
 
 
 if __name__ == '__main__':
